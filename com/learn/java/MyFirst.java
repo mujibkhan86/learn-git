d30f176efc8968e5c0b08aff0ed3d274ac2807e4
mujibkhan86 <mujibkhan86@gmail.com>
2024-09-19 23:06:38 +0530

    my new file

--- com/learn/java/MyFirst.java
+++ com/learn/java/MyFirst.java
@@ -3,5 +3,6 @@ package com.learn.java;
 public class MyFirst {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("java");
     }
 }
